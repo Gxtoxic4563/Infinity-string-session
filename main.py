e70a04274cddb801676bc2850af7959b0a86979b
Gxtoxic4563 <gxtoxic211@gmail.com>
2025-06-01 00:07:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import config
 import time
 import logging
 from pyrogram import Client, idle
-from pyromod import listen  # type: ignore
+from pyromod import listen  
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
 logging.basicConfig(
