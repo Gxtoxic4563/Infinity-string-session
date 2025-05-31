0457b0261302c200c85b4ccf74bd9df311e735a5
Gxtoxic4563 <gxtoxic211@gmail.com>
2025-06-01 00:00:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,17 +12,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # -------------------------------------------------------------
-API_ID = "22084906"
+API_ID = "24753274"
 # -------------------------------------------------------------
-API_HASH = "4ea89573b2ffa4b66154c7cf8c6fdd48"
+API_HASH = "625668050f7e193a994e2f5ddc4aafe5"
 # -------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("8071487525:AAFHaPViotLjQsLiE2AJ4c0Cej59zDLQZE0")
 # -------------------------------------------------------------
-OWNER_ID = getenv("OWNER_ID", 7009601543)
+OWNER_ID = getenv("OWNER_ID", "7290350162")
 # -------------------------------------------------------------
-MONGO_DB_URI = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
+MONGO_DB_URI = "mongodb+srv://mikey:mikey@cluster0.zv6knh9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # -------------------------------------------------------------
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PBX_CHAT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/infinitygx_bot_support")
 # -------------------------------------------------------------
 MUST_JOIN = getenv("MUST_JOIN","PBX_CHAT")
 # -------------------------------------------------------------
